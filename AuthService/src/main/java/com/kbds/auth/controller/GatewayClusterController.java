79614e0335f8567bbb77f183550ee5b7642f6735
kbfg195 <kookyungte@naver.com>
2020-06-24 10:50:14 +0900

    Intellij 테스트 2

--- AuthService/src/main/java/com/kbds/auth/controller/GatewayClusterController.java
+++ AuthService/src/main/java/com/kbds/auth/controller/GatewayClusterController.java
@@ -9,7 +9,21 @@ import org.springframework.web.bind.annotation.RestController;
 import com.kbds.auth.service.GatewayClusterService;
 import com.kbds.auth.utils.CommonUtils;
 
-
+/**
+ *
+ * <pre>
+ *  Class Name     : GatewayClusterController.java
+ *  Description    : Gateway 클러스터 관련 API 컨트롤러
+ *  Author         : 구경태 (kyungtae.koo@kbfg.com)
+ *
+ * -------------------------------------------------------------------------------
+ *     변경No        변경일자        	       변경자          Description
+ * -------------------------------------------------------------------------------
+ *     Ver 1.0      2020-05-04     구경태          Initialized
+ * -------------------------------------------------------------------------------
+ * </pre>
+ *
+ */
 @RestController
 @RequestMapping("/auth/")
 public class GatewayClusterController {
