27628fe018f5fae25b1a934e357a85271af21816
kbfg195 <kookyungte@naver.com>
2020-09-23 10:42:07 +0900

    2020.09.22
    1. 1차 프리징
    2. 포탈 프론트 작업 전 상태 저장

---
+++ Gateway/src/main/java/com/kbds/gateway/factory/GrantType.java
@@ -0,0 +1,25 @@
+package com.kbds.gateway.factory;
+
+import java.util.Map;
+
+/**
+ * <pre>
+ *  File  Name     : GrantType
+ *  Description    : Oauth GrantType Interface
+ *  Author         : 구경태 (kyungtae.koo@kbfg.com)
+ *
+ * -------------------------------------------------------------------------------
+ *     변경No        변경일자        	       변경자          Description
+ * -------------------------------------------------------------------------------
+ *     Ver 1.0      2020-09-20         	   구경태          Initialized
+ * -------------------------------------------------------------------------------
+ *  </pre>
+ */
+public interface GrantType {
+
+  /**
+   * GrantType별 Parameter 체크 메소드
+   * @param params  Body 전문
+   */
+  void validateParameters(Map<String, String> params);
+}
---
+++ Gateway/src/main/java/com/kbds/gateway/factory/GrantTypeFactory.java
@@ -0,0 +1,42 @@
+package com.kbds.gateway.factory;
+
+import com.kbds.gateway.code.GatewayExceptionCode;
+import com.kbds.gateway.exception.GatewayException;
+import org.springframework.http.HttpStatus;
+
+/**
+ * <pre>
+ *  File  Name     : GrantTypeFactory
+ *  Description    : GrantType Factory 객체
+ *  Author         : 구경태 (kyungtae.koo@kbfg.com)
+ *
+ * -------------------------------------------------------------------------------
+ *     변경No        변경일자        	       변경자          Description
+ * -------------------------------------------------------------------------------
+ *     Ver 1.0      2020-09-20          	 구경태          Initialized
+ * -------------------------------------------------------------------------------
+ *  </pre>
+ */
+public class GrantTypeFactory {
+
+  final String CONST_PASSWORD_TYPE = "password";
+  final String CONST_REFRESH_TOKEN_TYPE = "refresh_token";
+
+  /**
+   * GrantType에 따라 생성자를 다르게 전달해준다.
+   * @param grantType Oauth GrantType
+   * @return  GrantType 객체
+   */
+  public GrantType makeGrantType(String grantType) {
+
+    switch (grantType) {
+
+      case CONST_PASSWORD_TYPE:
+        return new PasswordGrantType();
+      case CONST_REFRESH_TOKEN_TYPE:
+        return new RefreshGrantType();
+      default:
+        throw new GatewayException(GatewayExceptionCode.GWE002, HttpStatus.UNAUTHORIZED);
+    }
+  }
+}
