4dd4a93db099474864a638aa98c0b879172fa68c
kbfg195 <kookyungte@naver.com>
2021-02-04 10:33:08 +0900

    2021.02.05
    1. ConfigServer 암호화
     - git 관련 정보 우선 암호화

--- GatewayConfig/src/main/java/com/kbds/config/GatewayConfigApplication.java
+++ GatewayConfig/src/main/java/com/kbds/config/GatewayConfigApplication.java
@@ -1,11 +1,13 @@
 package com.kbds.config;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEncryptableProperties
 public class GatewayConfigApplication {
 
   public static void main(String[] args) {
