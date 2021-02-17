434840d0e6e42aabdcdc3824e1a99976b39f6b6b
kbfg195 <kookyungte@naver.com>
2021-02-18 08:41:10 +0900

    2021.02.05
    1. ConfigServer 암호화
     - git 관련 정보 우선 암호화
    2. yml 파일 수정

---
+++ GatewayConfig/src/main/java/com/kbds/config/server/JasyptConfig.java
@@ -0,0 +1,29 @@
+package com.kbds.config.server;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptConfig {
+
+  final static String KEY = "kookyungte@naver.com";
+  final static String ALGORITHM = "PBEWithMD5AndDES";
+
+  @Bean("jasyptStringEncryptor")
+  public StringEncryptor stringEncryptor() {
+    PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+    SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+    config.setPassword(KEY);
+    config.setAlgorithm(ALGORITHM);
+    config.setKeyObtentionIterations("1000");
+    config.setPoolSize("1");
+    config.setProviderName("SunJCE");
+    config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+    config.setStringOutputType("base64");
+    encryptor.setConfig(config);
+    return encryptor;
+  }
+}
