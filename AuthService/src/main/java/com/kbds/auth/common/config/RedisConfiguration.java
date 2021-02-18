929921b8a29671a35acca0b62bf3317ce31302c4
kbfg195 <kookyungte@naver.com>
2021-02-18 10:03:33 +0900

    2021.02.05
    1. ConfigServer 암호화
     - git 관련 정보 우선 암호화
    2. yml 파일 수정

--- AuthService/src/main/java/com/kbds/auth/common/config/RedisConfiguration.java
+++ AuthService/src/main/java/com/kbds/auth/common/config/RedisConfiguration.java
@@ -1,6 +1,7 @@
 package com.kbds.auth.common.config;
 
 import com.kbds.auth.security.entity.OAuthAccessToken;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -24,20 +25,26 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
  */
 @Configuration
 public class RedisConfiguration {
-    
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
 
-        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory();
-        return lettuceConnectionFactory;
-    }
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
+  @Value("${spring.redis.host}")
+  private String redisHost;
 
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer(OAuthAccessToken.class));
-        return redisTemplate;
-    }
+  @Value("${spring.redis.port}")
+  private int redisPort;
+
+  @Bean
+  public RedisConnectionFactory redisConnectionFactory() {
+
+    return new LettuceConnectionFactory(redisHost, redisPort);
+  }
+
+  @Bean
+  public RedisTemplate<String, Object> redisTemplate() {
+
+    RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+    redisTemplate.setConnectionFactory(redisConnectionFactory());
+    redisTemplate.setKeySerializer(new StringRedisSerializer());
+    redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer(OAuthAccessToken.class));
+    return redisTemplate;
+  }
 }
